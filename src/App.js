440ad44e3d66a92de543644f813e629d2fcea757
Prabash <rprabash076@gmail.com>
2025-01-18 12:21:53 +0530

    ''

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ class App extends Component{
 render(){
   return(
     <div>
-      <h1 className="hello">Prabash</h1>
+      <h1 className="hello">Prabash {20+30}</h1>
       <p data-demoAttribute="demo">Welcome to React Js</p>
     </div>
   );
